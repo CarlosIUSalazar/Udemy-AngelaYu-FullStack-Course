5466fa27f9c72b117caacc0150bf29269b28e2b1
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-28 16:25:44 +0900

    Intro to Nodejs and NPM

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+let mySuperheroName = superheroes.random();
+
+let heroesList = [];
+for (let i = 0; i <= 1000000; i++){
+    heroesList.push(supervillains.random())
+}
+
+let heroToCheck = heroesList[1];
+let index = 0
+for (let i = 1; i <= heroesList.length; i++){
+    if (heroToCheck === heroesList[i]){
+        index++
+    }
+}
+
+console.table(heroesList);
+console.log(heroToCheck + " Appeared " + index + " Times");
