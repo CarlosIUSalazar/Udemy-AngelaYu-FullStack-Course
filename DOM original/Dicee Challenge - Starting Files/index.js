8ea05bdbae81cf273bd3b8d820dab5b07cd3914f
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-28 15:13:24 +0900

    Added folders

---
+++ DOM original/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,46 @@
+var random1 = Math.floor(Math.random()*6)+1;
+var random2 = Math.floor(Math.random()*6)+1;
+
+if (random1 == 1) {
+ document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+} else if (random1 == 2) {
+ document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+} else if (random1 == 3) {
+ document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+} else if (random1 == 4) {
+ document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+} else if (random1 == 5) {
+ document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+} else if (random1 == 6) {
+ document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
+
+if (random2 == 1) {
+ document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+} else if (random2 == 2) {
+ document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+} else if (random2 == 3) {
+ document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+} else if (random2 == 4) {
+ document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+} else if (random2 == 5) {
+ document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+} else if (random2 == 6) {
+ document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
+
+
+if (random1 > random2){
+ document.querySelector("h1").innerHTML=("🚩Player 1 Wins!")
+} else if (random2 > random1) {
+ document.querySelector("h1").innerHTML=("Player 2 Wins🚩")
+} else if (random1 === random2){
+ document.querySelector("h1").innerHTML=("It's a Draw")
+}
+
+
+
+// function randomIntFromInterval(min,max) // min and max included
+// {
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }
