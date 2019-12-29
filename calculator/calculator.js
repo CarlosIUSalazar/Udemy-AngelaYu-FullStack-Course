5dfb4b4c034ea7df6ee4f9c0d924aec5dfbe604b
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-29 17:51:24 +0900

    Added Body Parser

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+    res.send("thanks for sending that")
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
