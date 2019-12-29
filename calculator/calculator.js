7a818fcf3e88442e45789597d76b331105b4f2e2
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-29 18:17:32 +0900

    working calculator web app

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true})); //To pass data from the html form. Extended allows to post nested objects
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-    res.send("thanks for sending that")
+
+    console.log(req.body);
+    let num1 = Number(req.body.num1); // Number() converts string to number
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+    res.send("Teh result of the calculation is " + result)
 });
 
 app.listen(3000, function(){
