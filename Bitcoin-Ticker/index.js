40b63a7d7dd3f083624d65927b495033b56a7601
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-02 22:37:30 +0900

    Add JSON parser

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -16,14 +16,18 @@ app.post("/", function(req,res){
     //console.log(req.body.crypto);
 
     request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error,response,body){
+        
+        let data = JSON.parse(body);
+        let price = data.averages.week;  //last is the name of the key value we are looking for (latest value of btc)
+        console.log(price);
         //console.log(response);  //Brings a lot of data
         //console.log(response.statusCode);  //Retruns 200 which means request was successful. Check other HTTP Status Codes here: https://httpstatuses.com
-        console.log(body);
+        //console.log(body);
     });
 
 });
 
 
-app.listen(2000, function() {
+app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
