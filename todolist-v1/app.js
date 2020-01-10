e399b78954e2854693c8efa6e9fdcf8281a0b89e
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-10 16:06:11 +0900

    Add header footer and about ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,6 +5,7 @@ const app = express();
 //https://github.com/mde/ejs/wiki/Using-EJS-with-Express
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");  //place this line below const app = express();
 
@@ -26,19 +27,36 @@ app.get("/", function (req, res) {
 
 
     res.render("list", { 
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items 
     });    //is gonna look for folder views and file list.ejs
+});
+
+
+
+app.post("/", function(req,res){
+
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
 
-app.post("/", function(req,res){
-    let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
-})
 
 
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req,res) {
+    res.render("about");
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
